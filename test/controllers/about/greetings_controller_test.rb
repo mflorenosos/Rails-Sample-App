eddfb35b8654354dc43a8063148d1dba31393d71
Ricky Hurtado <odatruh.ykcir@gmail.com>
2016-12-30 15:51:03 +0800

    Move greetings to about module

--- test/controllers/about/greetings_controller_test.rb
+++ test/controllers/about/greetings_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class GreetingsControllerTest < ActionDispatch::IntegrationTest
+class About::GreetingsControllerTest < ActionDispatch::IntegrationTest
   test 'should get the about greetings page' do
-    get morning_greetings_url
+    get about_greetings_url
   	assert_response 200
   end
 
